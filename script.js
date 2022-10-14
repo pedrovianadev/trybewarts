772a5ff859db1d61a738a23e4dfc1af51be21d61
Pedro Viana <pedroviana.developer13@gmail.com>
2022-10-14 18:11:38 -0300

    arrumando lint

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const loginButton = document.getElementById('loginButton');
 
 loginButton.addEventListener('click', () => {
-  if (document.getElementById('email').value === 'tryber@teste.com' && document.getElementById('password').value === '123456') {
+  if (
+    document.getElementById('email').value === 'tryber@teste.com'
+    && document.getElementById('password').value === '123456'
+  ) {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
