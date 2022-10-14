0b84a0c5d7e34b0537f53b9d599bb289a35b16cc
Pedro Viana <pedroviana.developer13@gmail.com>
2022-10-14 15:23:03 -0300

    requisito 3 feito

---
+++ script.js
@@ -0,0 +1,7 @@
+loginButton.addEventListener('click', () => {
+  if (document.getElementById('email').value === 'tryber@teste.com' && document.getElementById('password').value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
