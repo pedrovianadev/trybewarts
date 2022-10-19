0782a6ccacdb0c6b0c1e5a3efcc9fec94fa1de74
Pedro Viana <pedroviana.developer13@gmail.com>
2022-10-19 10:56:13 -0300

    requisito 20 feito

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const disable = document.querySelector('#submit-btn');
 const loginButton = document.getElementById('loginButton');
+const character = document.querySelector('#textarea');
 
 disable.setAttribute('disabled', true);
 
@@ -20,4 +21,12 @@ agree.addEventListener('click', () => {
   if (agree.checked) {
     disable.removeAttribute('disabled');
   }
+});
+
+character.addEventListener('input', () => {
+  const qtdC = character.value.length;
+  const counter = document.querySelector('#counter');
+  const sub = 500 - qtdC;
+
+  counter.textContent = sub;
 });
